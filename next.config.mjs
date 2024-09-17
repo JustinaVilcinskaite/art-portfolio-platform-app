7551c2d5c1eb8504d0b2e16a06bc8282f6819f32
Justina Vilcinskaite <justinavilcinskaite@Justinas-MBP-2.lan>
2024-09-17 19:53:53 +0300

    updated server url

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    SERVER_URL: "http://localhost:3002",
+    // SERVER_URL: "http://localhost:3002",
+    SERVER_URL: "https://art-portfolio-platform-api.onrender.com",
     JWT_KEY: "portfolio_app_jwt",
   },
 };
